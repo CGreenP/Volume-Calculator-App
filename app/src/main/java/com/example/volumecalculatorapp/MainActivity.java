ab2eaa8bb866f5231343f9deb439f6b842ff01c7
Chayan Pal <chayan110201@gmail.com>
2024-08-02 18:13:03 +0530

    Status bar made transparent

--- app/src/main/java/com/example/volumecalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/volumecalculatorapp/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
